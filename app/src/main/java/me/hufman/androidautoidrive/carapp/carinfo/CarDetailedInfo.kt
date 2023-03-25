188f0997ba0083c4b808f7e96f905e4020421481
Katana1234 <tommyvercetti7777+github@gmail.com>
2023-03-25 18:30:28 +0100

    heading/direction fix

--- app/src/main/java/me/hufman/androidautoidrive/carapp/carinfo/CarDetailedInfo.kt
+++ app/src/main/java/me/hufman/androidautoidrive/carapp/carinfo/CarDetailedInfo.kt
@@ -1,11 +1,14 @@
 package me.hufman.androidautoidrive.carapp.carinfo
 
+import android.icu.number.NumberFormatter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.map
 import me.hufman.androidautoidrive.carapp.L
 import me.hufman.androidautoidrive.cds.CDSMetrics
 import me.hufman.androidautoidrive.cds.CDSVehicleUnits
+import me.hufman.androidautoidrive.phoneui.FlowUtils.addContextUnit
 import me.hufman.androidautoidrive.phoneui.FlowUtils.addPlainUnit
 import me.hufman.androidautoidrive.phoneui.FlowUtils.format
 
@@ -35,47 +38,114 @@ class CarDetailedInfo(cdsMetrics: CDSMetrics) {
 	}
 
 	// data points
-	val evLevelLabel = cdsMetrics.evLevel.format("%.1f%%").map { "$it ${L.CARINFO_EV_BATTERY}"}
+	val evLevelLabel = cdsMetrics.evLevel.format("%.1f%%").map { "$it ${L.CARINFO_EV_BATTERY}" }
 
-	val fuelLevelLabel = cdsMetrics.fuelLevel.format("%.1f").addPlainUnit(unitsFuelLabel).map { "$it ${L.CARINFO_FUEL}"}
+	val fuelLevelLabel = cdsMetrics.fuelLevel.format("%.1f").addPlainUnit(unitsFuelLabel)
+		.map { "$it ${L.CARINFO_FUEL}" }
 
-	val accBatteryLevelLabel = cdsMetrics.accBatteryLevel.format("%.0f%%").map { "$it ${L.CARINFO_ACC_BATTERY}"}
+	val accBatteryLevelLabel =
+		cdsMetrics.accBatteryLevel.format("%.0f%%").map { "$it ${L.CARINFO_ACC_BATTERY}" }
 
-	val engineTemp = cdsMetrics.engineTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_ENGINE}"}
-	val oilTemp = cdsMetrics.oilTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_OIL}"}
-	val batteryTemp = cdsMetrics.batteryTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_BATTERY}"}
+	val engineTemp = cdsMetrics.engineTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_ENGINE}" }
+	val oilTemp = cdsMetrics.oilTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_OIL}" }
+	val batteryTemp = cdsMetrics.batteryTemp.format("%.0f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_BATTERY}" }
 
-	val tempInterior = cdsMetrics.tempInterior.format("%.1f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_INTERIOR}"}
-	val tempExterior = cdsMetrics.tempExterior.format("%.1f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_EXTERIOR}"}
-	val tempExchanger = cdsMetrics.tempExchanger.format("%.1f").addPlainUnit(unitsTemperatureLabel).map { "$it ${L.CARINFO_EXCHANGER}"}
+	val tempInterior = cdsMetrics.tempInterior.format("%.1f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_INTERIOR}" }
+	val tempExterior = cdsMetrics.tempExterior.format("%.1f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_EXTERIOR}" }
+	val tempExchanger = cdsMetrics.tempExchanger.format("%.1f").addPlainUnit(unitsTemperatureLabel)
+		.map { "$it ${L.CARINFO_EXCHANGER}" }
 
-	val drivingMode = cdsMetrics.drivingMode
-	val drivingGearLabel = cdsMetrics.drivingGearName.map { "${L.CARINFO_GEAR} $it"}
+	val drivingMode = cdsMetrics.drivingMode.map { "Mode: $it" }
+	val drivingGearLabel = cdsMetrics.drivingGearName.map { "${L.CARINFO_GEAR}: $it" }
+
+	val speedActualLabel = cdsMetrics.speedActual.map { "Speed act.: ${it.toInt()}km/h" }
+	val rpmLabel = cdsMetrics.engineRpm.map { "RPM: $it" }
 
 	// driving detail fields, as examples
 	// real ones would need translated labels
-	val accelContact = cdsMetrics.accelerator.format("%.1f%%").map { "Accel $it"}
-	val accelEcoContact = cdsMetrics.acceleratorEco.format("%.1f%%").map { "AccelEco $it"}
-	val clutchContact = cdsMetrics.clutch.format("%.1f%%").map { "Clutch $it"}
-	val brakeContact = cdsMetrics.brake.format("%.1f%%").map { "Brake $it"}
-	val steeringAngle = cdsMetrics.steeringAngle.format("%.1f°").map { "Steering $it" }
+	val accelContact = cdsMetrics.accelerator.map { "Accel: $it%" }
+
+	//val accelEcoContact = cdsMetrics.acceleratorEco.format("%.1f%%").map { "AccelEco $it" }
+	//val clutchContact = cdsMetrics.clutch.map { "Clutch $it" }
+	//val brakeContact = cdsMetrics.brake.map { "Brake: $it" }
+	//val steeringAngle = cdsMetrics.steeringAngle.format("%.1f°").map { "Steering $it" }
+
+
+	val brakeInfoLabel = cdsMetrics.brakeInfo.map { "Brake: $it" }
+
+	val torqueConverterClutchLabel = cdsMetrics.clutchInfo.map { "Clutch: $it" }
+	val steeringWheelLabel = cdsMetrics.steeringWheel.map { "Steering: $it" }
+
+	val countryLabel = cdsMetrics.gpsCountry.map { "Country: $it" }
+	val cityLabel = cdsMetrics.gpsCity.map { "City: $it" }
+	val streetLabel = cdsMetrics.gpsStreet.map { "Street: $it" }
+	val crossStreetLabel = cdsMetrics.gpsCrossStreet.map { "X-Street: $it" }
+	val houseNumberLabel = cdsMetrics.gpsHouseNumber.map { "House number: $it" }
+	val headingLabel = cdsMetrics.gpsHeading.map { "Heading: $it°" }
+	val directionLabel = cdsMetrics.gpsDirection.map { "Direction: $it" }
+	val altitudeLabel = cdsMetrics.gpsAltitude.map { "Altitude: ${it}m" }
+	val latitudeLabel = cdsMetrics.gpsLat.map { "Latitude: $it" }
+	val longitudeLabel = cdsMetrics.gpsLon.map { "Longitude: $it" }
+
+	val accLonLabel = cdsMetrics.accLon.format("%+.1f").map { "Accel. Lon.: ${it}G" }
+	val accLatLabel = cdsMetrics.accLat.format("%+.1f").map { "Accel. Lan.: ${it}G" }
+
+	val windowDriverFrontStateLabel = cdsMetrics.windowDriverFrontState.map { "Driver front: $it" }
+	val windowPassengerFrontStateLabel = cdsMetrics.windowPassengerFrontState.map { "Passenger front: $it" }
+	val windowDriverRearStateLabel = cdsMetrics.windowDriverRearState.map { "Driver rear: $it" }
+	val windowPassengerRearStateLabel = cdsMetrics.windowPassengerRearState.map { "Passenger rear: $it" }
+	val sunroofLabel = cdsMetrics.sunRoof.map { "Sunroof: $it" }
 
 	// categories
 	private val overviewFields: List<Flow<String>> = listOf(
-			engineTemp, tempExterior,
-			oilTemp, tempInterior,
-			batteryTemp, tempExchanger,
-			fuelLevelLabel, evLevelLabel,
-			accBatteryLevelLabel, drivingGearLabel
+		engineTemp, tempExterior,
+		oilTemp, tempInterior,
+		batteryTemp, tempExchanger,
+		fuelLevelLabel, evLevelLabel,
+		accBatteryLevelLabel, emptyFlow()
 	)
+
 	private val drivingFields: List<Flow<String>> = listOf(
-			drivingMode, drivingGearLabel,
-			accelContact, accelEcoContact,
-			clutchContact, brakeContact,
-			steeringAngle
+		speedActualLabel, drivingMode,
+		rpmLabel, drivingGearLabel,
+		accelContact, torqueConverterClutchLabel,
+		brakeInfoLabel, accLonLabel,
+		steeringWheelLabel, accLatLabel
 	)
+
+	private val gpsFields: List<Flow<String>> = listOf(
+		countryLabel, directionLabel,
+		cityLabel, headingLabel,
+		streetLabel, altitudeLabel,
+		crossStreetLabel, latitudeLabel,
+		houseNumberLabel, longitudeLabel
+	)
+
+	private val windowsFields: List<Flow<String>> = listOf(
+		windowDriverFrontStateLabel, windowPassengerFrontStateLabel,
+		windowDriverRearStateLabel, windowPassengerRearStateLabel,
+		emptyFlow(), emptyFlow(),
+		sunroofLabel, emptyFlow()
+	)
+
+	/*
+	private val experimentalFields: List<Flow<String>> = listOf(
+		consumptionLabel, emptyFlow()
+	)
+	*/
+
+
 	val categories = LinkedHashMap<String, List<Flow<String>>>().apply {
 		put(L.CARINFO_TITLE, overviewFields)
+		put("Driving", drivingFields)
+		put("GPS", gpsFields)
+		put("Windows", windowsFields)
+		//put("Experimental", experimentalFields)
 
 		// add more pages like this:
 //		put("Driving Details", drivingFields)
--- app/src/main/java/me/hufman/androidautoidrive/cds/CDSMetrics.kt
+++ app/src/main/java/me/hufman/androidautoidrive/cds/CDSMetrics.kt
@@ -1,5 +1,6 @@
 package me.hufman.androidautoidrive.cds
 
+import com.google.gson.JsonObject
 import io.bimmergestalt.idriveconnectkit.CDS
 import kotlinx.coroutines.flow.*
 import me.hufman.androidautoidrive.CarInformation
@@ -7,6 +8,8 @@ import me.hufman.androidautoidrive.utils.GsonNullable.tryAsDouble
 import me.hufman.androidautoidrive.utils.GsonNullable.tryAsInt
 import me.hufman.androidautoidrive.utils.GsonNullable.tryAsJsonObject
 import me.hufman.androidautoidrive.utils.GsonNullable.tryAsJsonPrimitive
+import me.hufman.androidautoidrive.utils.GsonNullable.tryAsString
+import kotlin.math.absoluteValue
 import kotlin.math.max
 
 class CDSMetrics(val carInfo: CarInformation) {
@@ -15,12 +18,18 @@ class CDSMetrics(val carInfo: CarInformation) {
 	val units: Flow<CDSVehicleUnits> = carInfo.cachedCdsData.flow[CDS.VEHICLE.UNITS].map {
 		CDSVehicleUnits.fromCdsProperty(it)
 	}
-	val unitsAverageConsumption: Flow<CDSVehicleUnits.Consumption> = carInfo.cachedCdsData.flow[CDS.DRIVING.AVERAGECONSUMPTION].map {
-		CDSVehicleUnits.Consumption.fromValue(it.tryAsJsonObject("averageConsumption")?.getAsJsonPrimitive("unit")?.tryAsInt)
-	}
-	val unitsAverageSpeed: Flow<CDSVehicleUnits.Speed> = carInfo.cachedCdsData.flow[CDS.DRIVING.AVERAGESPEED].map {
-		CDSVehicleUnits.Speed.fromValue(it.tryAsJsonObject("averageSpeed")?.getAsJsonPrimitive("unit")?.tryAsInt)
-	}
+	val unitsAverageConsumption: Flow<CDSVehicleUnits.Consumption> =
+		carInfo.cachedCdsData.flow[CDS.DRIVING.AVERAGECONSUMPTION].map {
+			CDSVehicleUnits.Consumption.fromValue(
+				it.tryAsJsonObject("averageConsumption")?.getAsJsonPrimitive("unit")?.tryAsInt
+			)
+		}
+	val unitsAverageSpeed: Flow<CDSVehicleUnits.Speed> =
+		carInfo.cachedCdsData.flow[CDS.DRIVING.AVERAGESPEED].map {
+			CDSVehicleUnits.Speed.fromValue(
+				it.tryAsJsonObject("averageSpeed")?.getAsJsonPrimitive("unit")?.tryAsInt
+			)
+		}
 
 	/** data points */
 	// level
@@ -56,12 +65,14 @@ class CDSMetrics(val carInfo: CarInformation) {
 
 	// temp
 	val engineTemp = carInfo.cachedCdsData.flow[CDS.ENGINE.TEMPERATURE].mapNotNull {
-		it.tryAsJsonObject("temperature")?.tryAsJsonPrimitive("engine")?.tryAsDouble?.takeIf { it < 255 }
+		it.tryAsJsonObject("temperature")
+			?.tryAsJsonPrimitive("engine")?.tryAsDouble?.takeIf { it < 255 }
 	}.combine(units) { value, units ->
 		units.temperatureUnits.fromCarUnit(value)
 	}
 	val oilTemp = carInfo.cachedCdsData.flow[CDS.ENGINE.TEMPERATURE].mapNotNull {
-		it.tryAsJsonObject("temperature")?.tryAsJsonPrimitive("oil")?.tryAsDouble?.takeIf { it < 255 }
+		it.tryAsJsonObject("temperature")
+			?.tryAsJsonPrimitive("oil")?.tryAsDouble?.takeIf { it < 255 }
 	}.combine(units) { value, units ->
 		units.temperatureUnits.fromCarUnit(value)
 	}
@@ -84,9 +95,11 @@ class CDSMetrics(val carInfo: CarInformation) {
 	val tempExchanger = carInfo.cdsData.flow[CDS.CLIMATE.ACSYSTEMTEMPERATURES].mapNotNull {
 		it.tryAsJsonObject("ACSystemTemperatures")?.tryAsJsonPrimitive("heatExchanger")?.tryAsDouble
 	}
+	/*
 	val tempEvaporator = carInfo.cdsData.flow[CDS.CLIMATE.ACSYSTEMTEMPERATURES].mapNotNull {
 		it.tryAsJsonObject("ACSystemTemperatures")?.tryAsJsonPrimitive("evaporator")?.tryAsDouble
 	}
+	*/
 
 	/*
 		0 - Initialisierung -> initialization
@@ -123,7 +136,7 @@ class CDSMetrics(val carInfo: CarInformation) {
 		it == "Sport" || it == "Sport+" || it == "Race"
 	}
 	val drivingGear = carInfo.cdsData.flow[CDS.DRIVING.GEAR].mapNotNull {
-		it.tryAsJsonPrimitive("gear")?.tryAsInt?.takeIf { it >0 }
+		it.tryAsJsonPrimitive("gear")?.tryAsInt?.takeIf { it > 0 }
 	}
 	val drivingGearName = drivingGear.combine(drivingModeSport) { gear, isSporty ->
 		when (gear) {
@@ -139,6 +152,7 @@ class CDSMetrics(val carInfo: CarInformation) {
 		}
 	}
 
+
 	val speedActual = carInfo.cdsData.flow[CDS.DRIVING.SPEEDACTUAL].mapNotNull {
 		it.tryAsJsonPrimitive("speedActual")?.tryAsDouble
 	}.combine(units) { value, units ->
@@ -151,19 +165,252 @@ class CDSMetrics(val carInfo: CarInformation) {
 		units.distanceUnits.fromCarUnit(value)
 	}
 
+
 	val accelerator = carInfo.cdsData.flow[CDS.DRIVING.ACCELERATORPEDAL].mapNotNull {
-		it.tryAsJsonObject("acceleratorPedal")?.tryAsJsonPrimitive("position")?.tryAsDouble
+		it.tryAsJsonObject("acceleratorPedal")?.tryAsJsonPrimitive("position")?.tryAsInt
 	}
+	/*
 	val acceleratorEco = carInfo.cdsData.flow[CDS.DRIVING.ACCELERATORPEDAL].mapNotNull {
 		it.tryAsJsonObject("acceleratorPedal")?.tryAsJsonPrimitive("ecoPosition")?.tryAsDouble
 	}
+	*/
+	/*
 	val brake = carInfo.cdsData.flow[CDS.DRIVING.BRAKECONTACT].mapNotNull {
-		it.tryAsJsonPrimitive("brakeContact")?.tryAsDouble
+		it.tryAsJsonPrimitive("brakeContact")?.tryAsInt
 	}
+	*/
+	/*
 	val clutch = carInfo.cdsData.flow[CDS.DRIVING.CLUTCHPEDAL].mapNotNull {
-		it.tryAsJsonObject("clutchPedal")?.tryAsJsonPrimitive("position")?.tryAsDouble
+		it.tryAsJsonObject("clutchPedal")?.tryAsJsonPrimitive("position")?.tryAsInt
 	}
 	val steeringAngle = carInfo.cdsData.flow[CDS.DRIVING.STEERINGWHEEL].mapNotNull {
 		it.tryAsJsonObject("steeringWheel")?.tryAsJsonPrimitive("angle")?.tryAsDouble
 	}
+	*/
+
+
+	val brakeContact = carInfo.cdsData.flow[CDS.DRIVING.BRAKECONTACT].map {
+		it.tryAsJsonPrimitive("brakeContact")?.tryAsInt
+	}
+	val parkingBrake = carInfo.cachedCdsData.flow[CDS.DRIVING.PARKINGBRAKE].map {
+		it.tryAsJsonPrimitive("parkingBrake")?.tryAsInt
+	}
+	val parkingBrakeSet = parkingBrake.map {
+		it == 2 || it == 8 || it == 32
+	}
+	val brakeInfo = brakeContact.combine(parkingBrakeSet) { brakeContact, parkingBrakeSet ->
+		// brakeContact is a bit coded value
+		// bit 1 -> value 1 -> Brake pedal?
+		// bit 2 -> value 2 -> Soft braking
+		// bit 3 -> value 3 -> Medium / Strong braking
+		// bit 4 -> value 8 -> Cruise control is braking
+		// bit 5 -> value 16 -> Fullstop (when the vehicle is standing still, only when engine is running)
+
+		// Choosing the "most interesting" bit in the right order because multiple bits might be set
+		var brakeString = "-"
+		if (brakeContact?.and(8) ?: 0 == 8) {
+			brakeString = "Cruise Control"
+		} else if (brakeContact?.and(4) ?: 0 == 4) {
+			brakeString = "Strong"
+		} else if (brakeContact?.and(2) ?: 0 == 2) {
+			brakeString = "Soft"
+		}
+		// Adding the parking brake info
+		if (parkingBrakeSet) {
+			if (brakeString == "-") {
+				brakeString = "( ! )"
+			} else {
+				brakeString += " ( ! )"
+			}
+		}
+		brakeString
+	}
+
+	var clutchPedalPosition = carInfo.cdsData.flow[CDS.DRIVING.CLUTCHPEDAL].mapNotNull {
+		it.tryAsJsonObject("clutchPedal")?.tryAsJsonPrimitive("position")?.tryAsInt
+	}
+	var gearboxType = carInfo.cdsData.flow[CDS.ENGINE.INFO].mapNotNull {
+		it.tryAsJsonObject("info")?.tryAsJsonPrimitive("gearboxType")?.tryAsInt
+	}
+
+	val clutchInfo =
+		clutchPedalPosition.combine(gearboxType) { clutchPedalPosition, gearboxType ->
+			if (gearboxType == 1) {
+				//automatic transmission with torque converter
+				when (clutchPedalPosition) {
+					null -> ""
+					0 -> "Coupled"
+					1 -> "Sailing"
+					2 -> "Uncoupled"
+					3 -> "Open"
+					else -> "-$clutchPedalPosition-"
+				}
+			} else {
+				// unknown transmission type -> i don't know the values of clutchPedalPosition and gearboxType
+				// for manual transmissions, dual clutch transmissions and electric vehicles
+				"-$clutchPedalPosition-"
+			}
+		}
+
+	val steeringWheel = carInfo.cdsData.flow[CDS.DRIVING.STEERINGWHEEL].mapNotNull {
+		val a = it.tryAsJsonObject("steeringWheel")?.tryAsJsonPrimitive("angle")?.tryAsDouble
+		var dir = when {
+			a == null -> ""
+			a < 0 -> "R"
+			a > 0 -> "L"
+			else -> ""
+		}
+		val b = String.format("%.1f", a?.absoluteValue)
+		"$b°$dir"
+	}
+
+
+	val gpsCountry =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.CURRENTPOSITIONDETAILEDINFO].mapNotNull {
+			it.tryAsJsonObject("currentPositionDetailedInfo")
+				?.tryAsJsonPrimitive("country")?.tryAsString?.takeIf { it.isNotEmpty() }
+		}
+	val gpsCity =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.CURRENTPOSITIONDETAILEDINFO].mapNotNull {
+			it.tryAsJsonObject("currentPositionDetailedInfo")
+				?.tryAsJsonPrimitive("city")?.tryAsString?.takeIf { it.isNotEmpty() }
+		}
+	val gpsStreet =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.CURRENTPOSITIONDETAILEDINFO].mapNotNull {
+			it.tryAsJsonObject("currentPositionDetailedInfo")
+				?.tryAsJsonPrimitive("street")?.tryAsString?.takeIf { it.isNotEmpty() }
+		}
+	val gpsCrossStreet =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.CURRENTPOSITIONDETAILEDINFO].mapNotNull {
+			it.tryAsJsonObject("currentPositionDetailedInfo")
+				?.tryAsJsonPrimitive("crossStreet")?.tryAsString?.takeIf { it.isNotEmpty() }
+		}
+	val gpsHouseNumber =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.CURRENTPOSITIONDETAILEDINFO].mapNotNull {
+			it.tryAsJsonObject("currentPositionDetailedInfo")
+				?.tryAsJsonPrimitive("houseNumber")?.tryAsString?.takeIf { it.isNotEmpty() }
+		}
+
+	val gpsAltitude =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.GPSEXTENDEDINFO].mapNotNull {
+			it.tryAsJsonObject("GPSExtendedInfo")
+				?.tryAsJsonPrimitive("altitude")?.tryAsInt?.takeIf { it < 32767 }
+		}
+
+	val gpsLat =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.GPSPOSITION].mapNotNull {
+			it.tryAsJsonObject("GPSPosition")
+				?.tryAsJsonPrimitive("latitude")?.tryAsDouble
+		}
+	val gpsLon =
+		carInfo.cachedCdsData.flow[CDS.NAVIGATION.GPSPOSITION].mapNotNull {
+			it.tryAsJsonObject("GPSPosition")
+				?.tryAsJsonPrimitive("longitude")?.tryAsDouble
+		}
+
+	val gpsHeading = carInfo.cachedCdsData.flow[CDS.NAVIGATION.GPSEXTENDEDINFO].map {
+		var heading = it.tryAsJsonObject("GPSExtendedInfo")?.tryAsJsonPrimitive("heading")?.tryAsInt
+		if (heading != null) {
+			heading *= -1
+			heading += 360
+		}
+		heading
+	}
+	val gpsDirection = carInfo.cachedCdsData.flow[CDS.NAVIGATION.GPSEXTENDEDINFO].map {
+		var heading =
+			it.tryAsJsonObject("GPSExtendedInfo")?.tryAsJsonPrimitive("heading")?.tryAsDouble
+		if (heading != null) {
+			// heading defined in CCW manner, so we ned to invert to CW neutral direction wheel.
+			heading *= -1
+			heading += 360
+			//heading = -100 + 360  = 260;
+
+			if ((heading >= 0 && heading < 22.5) || (heading >= 347.5 && heading <= 360)) {
+				"N"
+			} else if (heading >= 22.5 && heading < 67.5) {
+				"NE"
+			} else if (heading >= 67.5 && heading < 112.5) {
+				"E"
+			} else if (heading >= 112.5 && heading < 157.5) {
+				"SE"
+			} else if (heading >= 157.5 && heading < 202.5) {
+				"S"
+			} else if (heading >= 202.5 && heading < 247.5) {
+				"SW"
+			} else if (heading >= 247.5 && heading < 302.5) {
+				"W"
+			} else if (heading >= 302.5 && heading < 347.5) {
+				"NW"
+			} else {
+				"-"
+			}
+		} else {
+			""
+		}
+	}
+
+	val accLon =
+		carInfo.cachedCdsData.flow[CDS.DRIVING.ACCELERATION].mapNotNull {
+			it.tryAsJsonObject("acceleration")
+				?.tryAsJsonPrimitive("longitudinal")?.tryAsDouble?.div(9.80665)// ms/s² to G
+		}
+
+	val accLat =
+		carInfo.cachedCdsData.flow[CDS.DRIVING.ACCELERATION].mapNotNull {
+			it.tryAsJsonObject("acceleration")
+				?.tryAsJsonPrimitive("lateral")?.tryAsDouble?.div(9.80665) // ms/s² to G
+		}
+
+	val engineRpm =
+		carInfo.cachedCdsData.flow[CDS.ENGINE.RPMSPEED].mapNotNull {
+			it.tryAsJsonPrimitive("RPMSpeed")?.tryAsInt
+		}
+
+	val sunRoof = carInfo.cachedCdsData.flow[CDS.CONTROLS.SUNROOF].map {
+		val status = it.tryAsJsonObject("sunroof")?.tryAsJsonPrimitive("status")?.tryAsInt ?: 0
+		val openPosition =
+			it.tryAsJsonObject("sunroof")?.tryAsJsonPrimitive("openPosition")?.tryAsInt ?: 0
+		val tiltPosition =
+			it.tryAsJsonObject("sunroof")?.tryAsJsonPrimitive("tiltPosition")?.tryAsInt ?: 0
+		if (status == 0 && tiltPosition == 0 && openPosition == 0) {
+			"Closed"
+		} else if (tiltPosition > 0 && openPosition == 0) {
+			"Tilted"
+		} else if (status == 1 && openPosition > 0) {
+			"Opened, ${openPosition * 2}%"
+		} else if (status == 2) {
+			"Fully opened"
+		} else {
+			""
+		}
+	}
+
+	private fun parseWindowStatus(data: JsonObject?): String {
+		val status = data?.tryAsJsonPrimitive("status")?.tryAsInt ?: 0
+		val position = data?.tryAsJsonPrimitive("position")?.tryAsInt ?: 0
+		return if (status == 0) {
+			"Closed"
+		} else if (status == 1) {
+			"Opened, ${position * 2}%"
+		} else {
+			"Fully opened"
+		}
+	}
+
+	val windowDriverFrontState = carInfo.cachedCdsData.flow[CDS.CONTROLS.WINDOWDRIVERFRONT].map {
+		parseWindowStatus(it.tryAsJsonObject("windowDriverFront"))
+	}
+	val windowPassengerFrontState =
+		carInfo.cachedCdsData.flow[CDS.CONTROLS.WINDOWPASSENGERFRONT].map {
+			parseWindowStatus(it.tryAsJsonObject("windowPassengerFront"))
+		}
+	val windowDriverRearState = carInfo.cachedCdsData.flow[CDS.CONTROLS.WINDOWDRIVERREAR].map {
+		parseWindowStatus(it.tryAsJsonObject("windowDriverRear"))
+	}
+	val windowPassengerRearState =
+		carInfo.cachedCdsData.flow[CDS.CONTROLS.WINDOWPASSENGERREAR].map {
+			parseWindowStatus(it.tryAsJsonObject("windowPassengerRear"))
+		}
+
+
 }
